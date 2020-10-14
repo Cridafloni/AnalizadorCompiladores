e822c659d75b61dbcb72910259cd68a65cd8adab
Cristian Flórez <cristiandflorez@hotmail.com>
2020-10-14 18:39:16 -0500

    Nueva clase observable

--- src/co/edu/uniquindio/compiladores/proyecto/controladores/InicioController.kt
+++ src/co/edu/uniquindio/compiladores/proyecto/controladores/InicioController.kt
@@ -1,14 +1,25 @@
 package co.edu.uniquindio.compiladores.proyecto.controladores
 
 import co.edu.uniquindio.compiladores.proyecto.lexico.AnalizadorLexico
+import co.edu.uniquindio.compiladores.proyecto.lexico.Categoria
+import co.edu.uniquindio.compiladores.proyecto.lexico.Token
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.TableColumn
 import javafx.scene.control.TextArea
 import javafx.scene.control.TextField
 
 class InicioController {
 
     @FXML lateinit var codigo:TextArea
+
+    @FXML lateinit var  tabla:ObservableList<Token>
+    @FXML lateinit var columnaLexema:TableColumn<String,String>
+    @FXML lateinit var columnaCategoria:TableColumn<Categoria,String>
+    @FXML lateinit var columnaFila:TableColumn<Int,String>
+    @FXML lateinit var columnaColumna:TableColumn<Int,String>
 
     @FXML
     fun analizarCodigo( e: ActionEvent){
@@ -18,6 +29,7 @@ class InicioController {
             lexico.analizar()
 
             print(lexico.listaTokens)
+
         }
     }
 }
