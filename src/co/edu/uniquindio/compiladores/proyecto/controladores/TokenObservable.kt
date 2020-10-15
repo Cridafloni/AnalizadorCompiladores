c08c527fac23f3b6c9ca65dece6b6dcb549e44da
Cristian Flórez <cristiandflorez@hotmail.com>
2020-10-14 19:17:07 -0500

    Se agregan nuevas funciones

--- src/co/edu/uniquindio/compiladores/proyecto/controladores/TokenObservable.kt
+++ src/co/edu/uniquindio/compiladores/proyecto/controladores/TokenObservable.kt
@@ -1,4 +1,14 @@
 package co.edu.uniquindio.compiladores.proyecto.controladores
 
+import javafx.beans.property.IntegerProperty
+import javafx.beans.property.StringProperty
+
 class TokenObservable {
+
+    lateinit var token:StringProperty
+    lateinit var categoria:StringProperty
+    lateinit var columna:IntegerProperty
+    lateinit var fila:IntegerProperty
+
+
 }
